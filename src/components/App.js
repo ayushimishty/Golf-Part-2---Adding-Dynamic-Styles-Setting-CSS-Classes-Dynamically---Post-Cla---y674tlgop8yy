653893efd88ff78341230f6b292d0339bf2a6591
Ayushi Jain <111980295+ayushimishty@users.noreply.github.com>
2022-12-19 00:00:31 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,54 @@
+
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+import { useEffect } from 'react';
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({ left: "0px", top: '0px' })
+  };
+
+  const renderChoice = () => {
+    if (renderBall === false) {
+      return <button onClick={() => setRenderBall(true)} className="start" >Start</button>
+    }
+    else if (renderBall === true) {
+      return <div className="ball" style={ballPosition}></div>
+    }
+  };
+  useEffect(() => {
+    // initiate the event handler
+    window.addEventListener("keydown", (e) => {
+      if (e.key === 'ArrowRight') {
+        setBallPosition({ left: x + 5 + "px", top: y + "px" });
+        setX(x + 5)
+      }
+      if (e.key === 'ArrowUp') {
+        setBallPosition({ left: x + "px", top: y - 5 + "px" });
+        setY(y - 5)
+      }
+      if (e.key === 'ArrowLeft') {
+        setBallPosition({ left: x - 5 + "px", top: y + "px" });
+        setX(x - 5)
+      }
+      if (e.key === 'ArrowDown') {
+        setBallPosition({ left: x + "px", top: y + 5 + "px" });
+        setY(y + 5)
+      }
+    },  false);
+
+    
+  });
 
   return (
     <div className="playground">
@@ -20,6 +58,7 @@ const App = () => {
       {renderChoice()}
     </div>
   );
-};
 
+
+}
 export default App;
